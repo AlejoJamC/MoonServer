ea080fff2e9ddc70925035998a355a083843fbce
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-30 10:57:53 -0500

    router setup

--- routes/routes.js
+++ routes/routes.js
@@ -5,3 +5,21 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+/**
+ * setupRouter
+ *
+ * @description  Configure all routes on express router
+ *
+ * @param {express}      app      The application server
+ */
+function setupRouter (app){
+    // Initialize all routes
+    var indexRoutes = require('./index');
+
+    // Assigned routes
+    // Index
+    app.use('/', indexRoutes);
+}
+// Export setup function
+module.exports.setupRouter = setupRouter;
