f44866caaba03f66e82dcd5ebf05c92fa40d8b21
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-30 11:53:14 -0500

    added TODO observations

---
+++ TODO.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// GLOBAL
+// TODO: agregar soporte para i18n-2
+
+// LOGIN
+// Default JS
+// TODO: alterar la funcion del javascript original para evitar que llame rutas fijas
