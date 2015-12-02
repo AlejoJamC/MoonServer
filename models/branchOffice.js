f1a04d028dd5dce8e48da7361f0b2fa71b97e123
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-12-02 02:42:18 -0500

    Branch Office model

---
+++ models/branchOffice.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// Load required packages
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Company = require('./company').Company;
+
+// Define Branch Office schema
+var BranchOfficeSchema = new Schema({
+    name:               String,
+    identification:     String,
+    address:            String,
+    telephone:          String,
+    email:              String,
+    contact:            String,
+    ContactTelephone:   String,
+    company:            [Company.schema],
+    creationDate:       Date,
+    lastEditionDate:    Date,
+    enabled:            Boolean
+});
+
+// Export the mongoose model
+module.exports.BranchOffice = mongoose.model('BranchOffice', BranchOfficeSchema);
