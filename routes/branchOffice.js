8cd4fd3b92ca9fac90d5f41e6f4e5ad0b89e2889
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-12-04 17:35:27 -0500

    Routes files for every entity

---
+++ routes/branchOffice.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+var express = require('express');
+var branchOfficeRouter = express.Router();
+
+
+/* GET Branch Office page. */
+branchOfficeRouter.get('/branch/office', function (req, res) {
+    res.render('branchOffice', {
+        title: 'Registro | Dynamite Docs',
+        level: '',
+        error: ''
+    });
+});
