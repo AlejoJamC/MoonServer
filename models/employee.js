8b1e55fb90a31715b24ca579f8537db92a91b4a9
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-09 17:03:01 -0500

    updated employee model

--- models/employee.js
+++ models/employee.js
@@ -7,45 +7,50 @@
  */
 
 // Load required packages
-var mongoose        = require('mongoose');
-var Schema          = mongoose.Schema;
+var mongoose                    = require('mongoose');
+var Schema                      = mongoose.Schema;
 
-var Company         = require('./company').Company;
-var BranchOffice    = require('./branchOffice').BranchOffice;
-var Department      = require('./department').Department;
-var Job             = require('./job').Job;
+var Company                     = require('./company').Company;
+var BranchOffice                = require('./branchOffice').BranchOffice;
+var Department                  = require('./department').Department;
+var Job                         = require('./job').Job;
+var HealthcareCompany           = require('./healthcareCompany').HealthcareCompany;
+var RetirementCompany           = require('./retirementCompany').RetirementCompany;
+var LaborRiskInsuranceCompany   = require('./laborRiskInsuranceCompany').LaborRiskInsurance;
+var CompensationFund         = require('./compensationFund').CompensationFund;
+
 
 // Define Employee schema
 var EmployeeSchema = new Schema({
-    name:                   String,
-    lastName:               String,
-    identification:         String,
-    identificationFile:     String,
-    photo:                  String,
-    address:                String,
-    telephone:              String,
-    email:                  String,
-    personalEmail:          String,
-    contact:                String,
-    ContactTelephone:       String,
-    curriculumFile:         String,
-    profession:             String,
-    company:                [Company.schema],
-    branchOffice:           [BranchOffice.schema],
-    department:             [Department.schema],
-    job:                    [Job.schema],
-    eps:                    String,
-    epsFile:                String,
-    pension:                String,
-    pensionFile:            String,
-    arl:                    String,
-    arlFile:                String,
-    compensationFund:       String,
-    compensationFundFile:   String,
-    salary:                 Number,
-    creationDate:           Date,
-    lastEditionDate:        Date,
-    enabled:                Boolean
+    name:                       String,
+    lastName:                   String,
+    identification:             String,
+    identificationFile:         String,
+    photo:                      String,
+    address:                    String,
+    telephone:                  String,
+    email:                      String,
+    personalEmail:              String,
+    contact:                    String,
+    ContactTelephone:           String,
+    curriculumFile:             String,
+    profession:                 String,
+    company:                    [Company.schema],
+    branchOffice:               [BranchOffice.schema],
+    department:                 [Department.schema],
+    job:                        [Job.schema],
+    healthcareCompany:          [HealthcareCompany.schema],
+    epsFile:                    String,
+    retirementCompany:          [RetirementCompany.schema],
+    retirementFile:             String,
+    laborRiskInsuranceCompany:  [LaborRiskInsuranceCompany.schema],
+    laborRiskInsuranceFile:     String,
+    compensationFund:           [CompensationFund.schema],
+    compensationFundFile:       String,
+    salary:                     Number,
+    creationDate:               Date,
+    lastEditionDate:            Date,
+    enabled:                    Boolean
 });
 
 // Export the mongoose model
