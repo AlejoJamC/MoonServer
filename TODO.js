7a2de0a3e8d35dbfe3b9098a014ae4df11f566af
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-09 17:40:47 -0500

    GLOBAL TODOs added

--- TODO.js
+++ TODO.js
@@ -8,7 +8,20 @@
 
 // GLOBAL
 // TODO: agregar soporte para i18n-2
+// TODO: Agregar en la documentacion la definicion del modelo de datos y si es aplicable al pais y como cambiarlo
 
 // LOGIN
 // Default JS
 // TODO: alterar la funcion del javascript original para evitar que llame rutas fijas
+
+// DASHBOARD
+// GLOBAL
+// TODO: Arreglar las rutas internas de navegacion de la seccion 'Header' Dashboard -> siguiente modulo -> caracteristica
+// TODO: Evaluar el uso de 'Actions' en el header
+
+// DASHBOARD > TIMELINE
+// TODO: Cambiar el sistemas de loguer por un sistema estructurado, -datetime -id relacional al evento - evento errores.
+// TODO: agregar el modelo de datos para el timeline
+
+// DASHBOARD > REGISTER
+// TODO: Arreglar el registro a 2 columnas o evitar que se vea innecesariamente estirado
