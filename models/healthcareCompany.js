a805e5671f4204bf081b72be2caf62eae9039ac8
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-09 16:41:09 -0500

    Models added EPS, ARL and PENSION

---
+++ models/healthcareCompany.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2016-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// Load required packages
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+
+// Define Healthcare Company schema
+var HealthcareCompanySchema = new Schema({
+    name:               String,
+    identification:     String,
+    address:            String,
+    telephone:          String,
+    email:              String,
+    contact:            String,
+    ContactTelephone:   String,
+    creationDate:       Date,
+    lastEditionDate:    Date,
+    enabled:            Boolean
+});
+
+// Export the mongoose model
+module.exports.HealthcareCompany = mongoose.model('HealthcareCompany', HealthcareCompanySchema);
