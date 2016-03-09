0ae92136cb7170078fd2b3c327bc3a9f70b9865d
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-08 21:29:53 -0500

    adding basic routes

--- routes/company.js
+++ routes/company.js
@@ -5,3 +5,49 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+var express = require('express');
+var companyRouter = express.Router();
+
+
+/* GET Branch Office page. */
+companyRouter.get('/companies', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+/* GET Create Branch Office page. */
+companyRouter.get('/companies/new', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+/* GET Update Branch Office page. */
+companyRouter.get('/companies/:id', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+/* GET Delete Branch Office page. */
+companyRouter.get('/companies/:id/delete', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../../../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+module.exports = companyRouter;
--- routes/job.js
+++ routes/job.js
@@ -5,3 +5,49 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+var express = require('express');
+var jobRouter = express.Router();
+
+
+/* GET Branch Office page. */
+jobRouter.get('/jobs', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+/* GET Create Branch Office page. */
+jobRouter.get('/jobs/new', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+/* GET Update Branch Office page. */
+jobRouter.get('/jobs/:id', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+/* GET Delete Branch Office page. */
+jobRouter.get('/jobs/:id/delete', function (req, res) {
+    res.render('dashboard/dashboard', {
+        title: 'Registro | Dynamite Docs',
+        level: '../../../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+module.exports = jobRouter;
