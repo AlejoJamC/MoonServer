4b3cc7e7be50dff7313c182c94ea589992f051c6
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-11 01:59:34 -0500

    route section created

--- routes/section.js
+++ routes/section.js
@@ -5,3 +5,19 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+var express = require('express');
+var sectionRouter = express.Router();
+
+/* GET Dashboard Timeline page. */
+// Route '/settings/sections'
+sectionRouter.get('/ajustes/secciones', function (req, res) {
+    res.render('dashboard/section', {
+        title: 'Secciones | Panel de control | Dynamite Docs',
+        level: '../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+module.exports = sectionRouter;
