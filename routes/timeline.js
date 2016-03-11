a460731adad45bb92b5ff80e34c1b25b12749c4e
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2016-03-11 01:59:53 -0500

    timeline section created

--- routes/timeline.js
+++ routes/timeline.js
@@ -5,3 +5,18 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+var express = require('express');
+var timelineRouter = express.Router();
+
+/* GET Dashboard Timeline page. */
+timelineRouter.get('/dashboard/timeline', function (req, res) {
+    res.render('dashboard/timeline', {
+        title: 'Timeline | Panel de control | Dynamite Docs',
+        level: '../',
+        layout: 'dashboard/mainStruct',
+        error: ''
+    });
+});
+
+module.exports = timelineRouter;
